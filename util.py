018e08a03e7046da50660e786e68227db90f8c5f
susanna9 <edens.s@husky.neu.edu>
2017-10-29 22:07:40 -0700

    extract data done

--- util.py
+++ util.py
@@ -10,14 +10,20 @@ SIXTEEN_BIT_MASK = 0xffff
 # type of message (ACK or DATA), the sequence number, the checksum value, and the payload. In
 # addition, it contains a boolean flag indicating bit corruption.
 class RDTPacket:
-  def __init__(self, msg_type=None, seq_num=None, checksum=None, payload=None, is_corrupt=True):
+  def __init__(self, msg_type, seq_num, checksum, payload, is_corrupt):
     self.msg_type = msg_type
     self.seq_num = seq_num
     self.checksum = checksum
     self.payload = payload
     self.is_corrupt = is_corrupt
 
+
+def get_corrupt_packet_representation():
+  return RDTPacket(None, None, None, None, False)
+
+
 ####################################################
+
 
 def get_checksum(pkt):
   checksum = 0
@@ -32,10 +38,10 @@ def get_checksum(pkt):
 
 def make_packet(msg, type, seq_num):
   bytelist = []
-  bytelist.append(struct.pack('!H', type))      # HEADER 1: MESSAGE TYPE
-  bytelist.append(struct.pack('!H', seq_num))   # HEADER 2: SEQUENCE NUMBER
-  bytelist.append(struct.pack('!H', 0))         # HEADER 3: CHECKSUM (append 0 for now)
-  bytelist.append(msg.encode())                 # The payload # TODO: will the message be given as string or bytes object?
+  bytelist.append(struct.pack('!H', type))     # HEADER 1: MESSAGE TYPE
+  bytelist.append(struct.pack('!H', seq_num))  # HEADER 2: SEQUENCE NUMBER
+  bytelist.append(struct.pack('!H', 0))        # HEADER 3: CHECKSUM (append 0 for now)
+  bytelist.append(msg.encode())                # The payload # TODO: will the message be given as string or bytes object?
 
   checksum = get_checksum(b''.join(bytelist))
   checksum_bytes = struct.pack("!H", checksum)
@@ -46,20 +52,12 @@ def make_packet(msg, type, seq_num):
   return packet
 
 
-# TODO: check that this works
 def extract_data(msg):
-  assert len(msg) >= 6
-  # TODO: if during extraction, the packet is found to be corrupt, return the packet below
-  corrupt_packet = RDTPacket() # TODO: try to put whatever data you have in there
-
-  # TODO: given a full packet, extract the payload from the message. Return as RDTPacket object
-  #packet = RDTPacket(msg_type, seq_num, checksum, payload, False)
-  #1. calculate the checksum
-  #2. compare the checksum to the 5-6bytes of the packet and see if they match
-  #3. iterate through the message: the first 3 2-byte chunks are the headers. the rest is the
-  # payload.
-  # assert that msg is at least 6 bytes
-  return corrupt_packet
+  # TODO: if it is corrupt, we shouldn't try to pull any data from it right?
+  if len(msg) < 6 or not get_checksum(msg) == 0:
+    return get_corrupt_packet_representation()
+  headers = struct.unpack("!3H", msg[0:6])
+  return RDTPacket(headers[0], headers[1], headers[2], msg[6:], False)
 
 
 def get_transport_layer_by_name(name, local_port, remote_port, msg_handler):
@@ -73,7 +71,17 @@ def get_transport_layer_by_name(name, local_port, remote_port, msg_handler):
 
 
 
-# s = '4500003044224000800600008c7c19acae241e2b'
-# bs = bytearray.fromhex(s)
-# c = get_checksum(bs)
-# print(c)
+
+# packet = make_packet("this is my message", config.MSG_TYPE_DATA, 1)
+# print(packet)
+# data = extract_data(packet)
+# print(data.msg_type)
+# print(data.seq_num)
+# print(data.checksum)
+# print(data.payload)
+# print(data.is_corrupt)
+#
+# # s = '4500003044224000800600008c7c19acae241e2b'
+# # bs = bytearray.fromhex(s)
+# # c = get_checksum(bs)
+# # print(c)
